7fab2d0e8b9daabc963dfdfade2b7a5f38d6ada9
Hihiz <public@mail.ru>
2026-02-24 03:23:08 +0300

    Добавить начальную инициализацию ролей

---
+++ AspNet.IdentityJwtRefreshExample/Data/SeedDataIdentityRole.cs
@@ -0,0 +1,33 @@
+﻿using AspNet.IdentityJwtRefreshExample.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNet.IdentityJwtRefreshExample.Data
+{
+    /// <summary>
+    /// Класс инициализирует таблицу ролей начальными данными.
+    /// </summary>
+    public class SeedDataIdentityRole
+    {
+        public static async Task SeedRoleAsync(IServiceProvider serviceProvider)
+        {
+            RoleManager<IdentityRole<long>> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<
+                long>>>();
+            
+            string[] roles = ["User", "Admin"];
+
+            foreach (var role in roles)
+            {
+                bool isRoleExist = await roleManager.RoleExistsAsync(role);
+
+                if (!isRoleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole<long>
+                    {
+                        Name = role,
+                        NormalizedName = role.ToUpper()
+                    });
+                }
+            }          
+        }
+    }
+}

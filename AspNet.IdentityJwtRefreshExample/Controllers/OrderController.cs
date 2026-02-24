6cf8b860bd1094fd5a3511b5d84f49e705a9d549
Hihiz <public@mail.ru>
2026-02-24 03:25:19 +0300

    Добавить контроллер заказов

---
+++ AspNet.IdentityJwtRefreshExample/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet.IdentityJwtRefreshExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        /// <summary>
+        /// Метод получения списка заказов.
+        /// Доступ для ролей "User".
+        /// </summary>
+        /// <returns>Список заказов.</returns>
+        [Authorize(Roles = "User, Admin")]
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            List<string> orders = ["order1", "order2", "order3"];
+
+            return Ok(orders);
+        }
+    }
+}

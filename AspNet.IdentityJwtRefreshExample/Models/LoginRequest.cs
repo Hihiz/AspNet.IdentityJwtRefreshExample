f2f6dcfdd94a61b0fec86ea56c53a36a285cdc44
Hihiz <public@mail.ru>
2026-02-24 03:17:55 +0300

    Добавить входную модель аутентификации

---
+++ AspNet.IdentityJwtRefreshExample/Models/LoginRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNet.IdentityJwtRefreshExample.Models
+{
+    /// <summary>
+    /// Класс входной модели аутентификации пользователя.
+    /// </summary>
+    public class LoginRequest
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}

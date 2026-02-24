a25dc2a4b05f3d80cdca6d3c13ee99333a4cbe19
Hihiz <public@mail.ru>
2026-02-24 03:21:53 +0300

    Добавить класс пользователя

---
+++ AspNet.IdentityJwtRefreshExample/Identity/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNet.IdentityJwtRefreshExample.Identity
+{
+    /// <summary>
+    /// Класс пользователя для авторизации.
+    /// </summary>
+    public class ApplicationUser : IdentityUser<long>
+    {
+        /// <summary>
+        /// Имя пользователя.
+        /// </summary>
+        public string? LastName { get; set; }
+
+        /// <summary>
+        /// Фамилия пользователя.
+        /// </summary>
+        public string? FirstName { get; set; }
+
+        /// <summary>
+        /// Токен обновления, для получения нового AccessToken.
+        /// </summary>
+        public string? RefreshToken { get; set; }
+
+        /// <summary>
+        /// Время жизни токена обновления.
+        /// </summary>
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}

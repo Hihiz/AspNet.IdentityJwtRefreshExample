fb1fd68ab4af10dc14088662fd937e515b9a76b9
Hihiz <public@mail.ru>
2026-02-24 03:22:38 +0300

    Добавить класс контекста

---
+++ AspNet.IdentityJwtRefreshExample/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AspNet.IdentityJwtRefreshExample.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNet.IdentityJwtRefreshExample.Data
+{
+    /// <summary>
+    /// Класс контекста Ef Core.
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options) { }
+    }
+}

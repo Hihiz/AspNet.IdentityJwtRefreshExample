f3de8bbfe96fb83f186aef84eb7500b48d5d4a8f
Hihiz <public@mail.ru>
2026-02-24 03:18:35 +0300

    Добавить выходную модель токена

---
+++ AspNet.IdentityJwtRefreshExample/Models/TokenResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNet.IdentityJwtRefreshExample.Models
+{
+    /// <summary>
+    /// Класс выходной модели токенов пользователя.
+    /// </summary>
+    public class TokenResponse
+    {
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}

61971d311798b02c717e6882342ca88fc44382f7
Hihiz <public@mail.ru>
2026-02-24 03:18:05 +0300

    Добавить входную модель регистрации

---
+++ AspNet.IdentityJwtRefreshExample/Models/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNet.IdentityJwtRefreshExample.Models
+{
+    /// <summary>
+    /// Класс входной модели регистрации пользователя.
+    /// </summary>
+    public class RegisterRequest
+    {
+        public string? LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? PasswordConfirm { get; set; }
+    }
+}
